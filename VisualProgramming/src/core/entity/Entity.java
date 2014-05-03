479815f71004acb5a40d22a7d351acd9e815e466
Kyran Adams <kyranstar@yahoo.com>
2014-05-02 19:24:10 -0700

    Updated Entity

--- VisualProgramming/src/core/entity/Entity.java
+++ VisualProgramming/src/core/entity/Entity.java
@@ -2,14 +2,24 @@ package core.entity;
 
 import java.awt.Graphics2D;
 
+import core.math.Vec2D;
+
 public abstract class Entity {
 
+	protected CollisionBox collisionBox;
+	protected Vec2D velocity;
 	protected boolean isDead;
+	
+	public Entity(int x, int y, int width, int height){
+		collisionBox = new CollisionBox(x, y, width, height);
+		velocity = new Vec2D(0,0);
+	}
 	
 	public abstract void draw(Graphics2D g);
 	public abstract void update();
 	public boolean isDead() {
 		return isDead;
 	}
+	public abstract void applyAcceleration(Vec2D accel);
 
 }
