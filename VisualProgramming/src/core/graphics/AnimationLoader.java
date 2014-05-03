848ae5616e6ec06659eb439f60576336cd89a6a2
Kyran Adams <kyranstar@yahoo.com>
2014-05-02 19:23:01 -0700

    Initial Commit

---
+++ VisualProgramming/src/core/graphics/AnimationLoader.java
@@ -0,0 +1,20 @@
+package core.graphics;
+
+import java.awt.image.BufferedImage;
+
+public final class AnimationLoader {
+	private AnimationLoader(){}
+	/*
+	 * Takes in 1 dimensional images with back to back sprites
+	 */
+	public static Animation getFromSpritesheet(String spritesheet, int spriteWidth) {
+		BufferedImage sheetImage = ImageLoader.loadImage(spritesheet);
+		BufferedImage[] images = new BufferedImage[sheetImage.getWidth() / spriteWidth];
+		for(int i = 0; i < images.length; i++)
+			images[i] = sheetImage.getSubimage(i * spriteWidth, 0, spriteWidth, sheetImage.getHeight());
+		Animation a = new Animation();
+		a.setFrames(images);
+		return a;
+	}
+
+}
--- VisualProgramming/src/core/graphics/ImageLoader.java
+++ VisualProgramming/src/core/graphics/ImageLoader.java
@@ -2,8 +2,22 @@ package core.graphics;
 
 import java.awt.image.BufferedImage;
 
-public class ImageLoader { //TODO implement image buffer  
+public final class ImageLoader { //TODO implement image buffer  
+	private ImageLoader(){}
+	
 	public static BufferedImage loadImage(String filename){
 		return null;
+	}
+	public static BufferedImage[][] loadTilesheet(String filename, int spriteWidth, int spriteHeight){
+		BufferedImage spritesheet = loadImage(filename);
+		int cols = spritesheet.getWidth() / spriteWidth;
+		int rows = spritesheet.getHeight() / spriteHeight;
+		BufferedImage[][] tiles = new BufferedImage[cols][rows];
+		for(int x = 0; x < cols; x++){
+			for(int y = 0; y < rows; y++){
+				tiles[x][y] = spritesheet.getSubimage(x * spriteWidth, y * spriteHeight, spriteWidth, spriteHeight);
+			}
+		}
+		return tiles;
 	}
 }
