53edd87aa06545e267f37b7b7c124ab5109a5a81
Kyran Adams <kyranstar@yahoo.com>
2014-05-06 21:55:27 -0700

    Started JUnit tests

---
+++ VisualProgramming/test/core/math/Vec2DTest.java
@@ -0,0 +1,14 @@
+package core.math;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Vec2DTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
