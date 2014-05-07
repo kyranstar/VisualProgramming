7619492385b4c9aacf1e741d70bd583ef002ce64
Kyran Adams <kyranstar@yahoo.com>
2014-05-07 14:46:43 -0700

    Added new block entity

---
+++ VisualProgramming/src/core/entity/neutral/BlockEntity.java
@@ -0,0 +1,33 @@
+package core.entity.neutral;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+import core.entity.AbstractEntity;
+import core.math.Vec2D;
+import core.object.map.GameMap;
+
+public class BlockEntity extends AbstractEntity{
+
+	public BlockEntity(int x, int y, double width, double height, GameMap map) {
+		super(map);
+		this.setRect(x, y, width, height);
+	}
+
+	@Override
+	public void draw(Graphics2D g) {
+		g.setColor(Color.RED);
+		g.fillRect((int)this.getX(), (int) this.getY(), (int)this.getSize().x, (int) this.getSize().y);
+	}
+
+	@Override
+	public void update() {
+		this.setPosition(this.getNextPosition());
+	}
+
+	@Override
+	public void applyAcceleration(Vec2D accel) {
+		this.setVelocity(this.getVelocity().add(accel));
+	}
+
+}
